64de0161c928430d5ea7a3a1d3735210934366b5
Matheus Augusto <148787099+matheus-assilva@users.noreply.github.com>
2024-01-29 13:49:16 -0300

    Add files via upload

---
+++ # Desenvolvimento 3.py
@@ -0,0 +1,20 @@
+##  Desenvolvimento 3
+
+# Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+
+# Escreva um código que imprima todos os números exceto o número 13.
+# Escreva mais um código que resolva o mesmo problema, mas dessa vez usando o laço de repetição 'while'.
+
+# Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
+
+# Usando for
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+# Usando while
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
